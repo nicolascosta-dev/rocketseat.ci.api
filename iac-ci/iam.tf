5b5ff026b8b01c2bb3c3afde306e7276007d5c53
nicolascosta-dev <nicobcosta@gmail.com>
2026-09-02 21:18:08 -0300

    fix: recriando recursos iac

--- iac-ci/iam.tf
+++ iac-ci/iam.tf
@@ -1,13 +1,12 @@
-# 1. Busca dinamicamente o certificado atual do GitHub
-data "tls_certificate" "github" {
-  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
-}
-
 resource "aws_iam_openid_connect_provider" "oidc-git" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  # Usa o thumbprint buscado no bloco de dados acima
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  
+  # Thumbprints oficiais do GitHub Actions suportados pela AWS
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
 
   tags = {
     iac = true
